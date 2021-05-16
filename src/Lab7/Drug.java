3ceb8928a54d0fb7ad65b8b8eb439503206c7779
Danylo Chelyuk <dchelyuk@gmail.com>
2021-05-16 19:52:30 +0300

    Added all constructors as well as links from one constructor to another

--- src/Lab7/Drug.java
+++ src/Lab7/Drug.java
@@ -1,27 +1,35 @@
 package Lab7;
 
 public class Drug {
-    String type = "pill";
+    int release_year = 2021;
 
     String name;
     int volume_of_active_substance;
     int weight_in_mg;
     String active_substance_name;
     int max_doses_per_day;
+    double duration;
     int count_in_package;
     double package_price;
     double pill_price;
 
-    Drug(String name, int volume_of_active_substance, int weight_in_mg, String active_substance_name,
-         int max_doses_per_day, int count_in_package, double package_price) {
+    Drug() {}
+
+    Drug(String name, double duration, int count_in_package, double package_price) {
         this.name = name;
+        this.duration = duration;
+        this.count_in_package = count_in_package;
+        this.package_price = package_price;
+        this.pill_price = package_price / count_in_package;
+    }
+
+    Drug(String name, int volume_of_active_substance, int weight_in_mg, String active_substance_name,
+         int max_doses_per_day, double duration, int count_in_package, double package_price) {
+        this(name, duration, count_in_package, package_price);
         this.volume_of_active_substance = volume_of_active_substance;
         this.weight_in_mg = weight_in_mg;
         this.active_substance_name = active_substance_name;
         this.max_doses_per_day = max_doses_per_day;
-        this.count_in_package = count_in_package;
-        this.package_price = package_price;
-        this.pill_price = package_price / count_in_package;
 
     }
 }
--- src/Lab7/Main.java
+++ src/Lab7/Main.java
@@ -3,11 +3,11 @@ package Lab7;
 public class Main {
     public static void main(String[] args) {
         Drug drug1 = new Drug("Ibuprofen", 100, 300, "Ibuprom",
-                3, 10, 10.00);
+                3, 2.5, 10, 10.00);
         Drug drug2 = new Drug("Nurofen", 20, 200, "Nurom",
-                20, 20, 15.00);
+                20, 5, 20, 15.00);
         Drug drug3 = new Drug("Validol", 50, 400, "Validol",
-                10, 5, 7.50);
+                10, 24, 5, 7.50);
         System.out.println(drug1.pill_price);
         System.out.println(drug2.pill_price);
         System.out.println(drug3.pill_price);
